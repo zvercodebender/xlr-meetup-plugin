4a88e2faddf6facd8d03fabaf47254c4125aa7e6
Admin <rick_broker@yahoo.com>
2020-06-16 15:53:38 -0400

    Add rest api

--- src/main/resources/meetup/Client.py
+++ src/main/resources/meetup/Client.py
@@ -8,8 +8,10 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 import random
+import json
 import org.slf4j.Logger as Logger
 import org.slf4j.LoggerFactory as LoggerFactory
+from xlrelease.HttpRequest import HttpRequest
 
 class Client(object):
     def __init__(self):
@@ -29,3 +31,18 @@ class Client(object):
         self.ranVal = random.randint( self.minVal, self.maxVal )
         self.logger.error("Random Number = %s" % self.ranVal)
         return {"output" : str(self.ranVal)}
+
+    def meetup_getpostcomments(self, variables):
+        httpConnection = variables['server']
+        httpRequest = HttpRequest(httpConnection, variables['username'], variables['password'])
+        url = "/comments?postId=%s" % variables['postId']
+        self.logger.error("Get Post Comments %s" % (url))
+        response = httpRequest.get(url, contentType='application/json')
+        jsonData = response.getResponse()
+        data = json.loads( jsonData )
+        listData = []
+        for recordData in data:
+            self.logger.error( recordData['body'] )
+            listData.append( recordData['body'] )
+        returnData = {"responseJson": jsonData, "comments": listData }
+        return returnData
